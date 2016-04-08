7b9c0392bfd6aabbd81fc985ea24876f46a33805
Dariusz Rybicki <darrarski@gmail.com>
2016-04-08 03:08:37 +0200

    Added convenience methods for saving or rolling back and grouping save operations

--- Sources/NSManagedObjectContext+SaveHelpers.swift
+++ Sources/NSManagedObjectContext+SaveHelpers.swift
@@ -64,4 +64,51 @@ public extension NSManagedObjectContext {
 
         try save()
     }
+    
+    /**
+     Convenience method to synchronously save the `NSManagedObjectContext` if changes are present.
+     If `save()` function on the `NSManagedObjectContext` produces error, all changes will be rolled back and the error thrown.
+     
+     - throws: Errors produced by the `save()` function on the `NSManagedObjectContext`
+     */
+    public func saveOrRollback() throws {
+        do {
+            try saveContextAndWait()
+        } catch {
+            rollback()
+            throw error
+        }
+    }
+    
+    /**
+     Convenience method to asynchronously save the `NSManagedObjectContext` if changes are present.
+     The method uses dispatch group to group multiple calls. Save will be delayed, so multiple calls to this method won't cause it 
+     untill maximum changed objects count will be exceeded. Save or rollback will be always performed on context's queue.
+     If `save()` function on the `NSManagedObjectContext` produces error, all changes will be rolled back and the error thrown.
+     
+     - parameter group: `dispatch_group_t` that will be used group calls to this method.
+     - parameter maxChangedObjectsCount: Maximum changed objects count, that if exceeded will cause save.
+     - parameter onError: A closure that will be called when `save()` function on the `NSManagedObjectContext` produces error.
+     */
+    public func saveOrRollbackWithGroup(group: dispatch_group_t, maxChangedObjectsCount: Int = 100, onError: (ErrorType) -> ()) {
+        guard changedObjectsCount < maxChangedObjectsCount else {
+            do { try saveOrRollback() }
+            catch { onError(error) }
+            return
+        }
+        let queue = dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)
+        dispatch_group_notify(group, queue) {
+            dispatch_group_enter(group)
+            self.performBlock {
+                guard self.hasChanges else { return }
+                do { try self.saveOrRollback() }
+                catch { onError(error) }
+                dispatch_group_leave(group)
+            }
+        }
+    }
+    
+    private var changedObjectsCount: Int {
+        return insertedObjects.count + updatedObjects.count + deletedObjects.count
+    }
 }
